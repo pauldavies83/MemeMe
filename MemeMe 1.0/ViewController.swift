ec2c29be677adb217f549987a60a61b469063f19
pauldavies83 <pauldavies83@googlemail.com>
2021-03-11 20:13:04 +0000

    add camera picker button and disable when no camera available

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -10,21 +10,34 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var pickedImage: UIImageView!
+    @IBOutlet weak var openCameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        openCameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
     
     @IBAction func pickImage(_ sender: Any) {
+        launchImagePicker(.photoLibrary)
+    }
+    
+    @IBAction func openCamera(_ sender: Any) {
+        launchImagePicker(.camera)
+    }
+    
+    func launchImagePicker(_ sourceType: UIImagePickerController.SourceType) {
         let picker: UIImagePickerController = UIImagePickerController()
         picker.delegate = self
+        picker.sourceType = sourceType
         present(picker, animated: true, completion: nil)
     }
     
+    
+    // MARK: UIImagePickerControllerDelegate implementation
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
         if let image = info[.originalImage] as? UIImage {
             pickedImage.image = image
         }
