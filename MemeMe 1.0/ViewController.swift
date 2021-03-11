c6d150a166ab5a47271c11415ef6ac5e9ea940e9
pauldavies83 <pauldavies83@googlemail.com>
2021-03-11 19:43:37 +0000

    add image picker and display picked image

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var pickedImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func pickImage(_ sender: Any) {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage {
+            pickedImage.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            dismiss(animated: true, completion: nil)
+    }
 }
 
