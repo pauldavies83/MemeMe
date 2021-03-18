fea42dba6662edd0497b6ac75f62e9db9059c1d2
pauldavies83 <pauldavies83@googlemail.com>
2021-03-18 09:21:14 +0000

    give ViewController a more descriptive name

--- MemeMe 1.0/MemeEditorViewController.swift
+++ MemeMe 1.0/MemeEditorViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate,
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate,
                       UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var topToolbar: UIToolbar!
